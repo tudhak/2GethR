3fe92726a50d639a16f36889dac41730a05fb8db
jeremy madaule <jeremy.madaule@gmail.com>
2023-11-28 12:41:43 +0100

    paramétrage mobile app 27-11

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Ajouté par JM pour le set up mobile app
+  config.hosts.clear
 end
