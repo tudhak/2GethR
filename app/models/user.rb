bc321a3e8e10221fddf61f994eddd74d09ea5717
erwan palanque <palanque.erwan@gmail.com>
2023-11-28 12:24:24 +0100

    fin des models avec message

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  belongs_to :couple
+  has_many :statues
+  has_many :rewards
+  has_many :tasks
+  has_many :messages
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
