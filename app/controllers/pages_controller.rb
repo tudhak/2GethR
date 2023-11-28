a7e74fd29622f84102a1341c62c6c2735b520569
jeremy madaule <jeremy.madaule@gmail.com>
2023-11-28 16:17:10 +0100

    commit before pull

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,26 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @user = current_user
   end
+
+  def score
+
+  end
+
+  private
+
+  def set_partner
+    @couple = Couple.find(current_user.couple_id)
+    @partner = User.where(@couple) - current_user
+  end
+
+  def score_user
+    @score_user = current_user.score
+  end
+
+  def score_partner
+    @score_partner = User.find(current_user.id)
+  end
+
 end
