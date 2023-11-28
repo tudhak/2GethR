9a2e4927fa3f127a7a32a92294a93fcc7c7eb9f0
Tudal Haccandy <tud.hak_alt@outlook.com>
2023-11-28 16:36:18 +0100

    added task seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_28_120044) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_28_145335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,6 +62,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_28_120044) do
     t.integer "base_score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
   create_table "messages", force: :cascade do |t|
