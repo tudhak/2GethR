5ef4f90ba086c91c043a52af7b87c90d7a581682
Tudal Haccandy <tud.hak_alt@outlook.com>
2023-11-29 18:16:02 +0100

    further attempts to task filtering

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,10 +2,21 @@ class TasksController < ApplicationController
   before_action :set_couple, only: [:index]
 
   def index
-    @tasks = Task.where(user: User.where(couple_id: params[:couple_id]))
+    @tasks = Task.where(user: User.where(couple_id: @couple.id))
     @my_pending_tasks = Task.where(assigned_to: current_user.nickname).where(statue: "pending").order(date: :desc)
     @partner = User.where(couple_id: current_user.couple_id).where.not(id: current_user.id).first
-    @filtered_tasks_assigned = @tasks.where(assigned_to: params[:assigned_to])
+    @task = Task.new
+
+    # raise
+
+    if params[:my_params].present?
+      @tasks = Task.where(assigned_to: params[:my_params][:assigned_to], statue: params[:my_params][:statue])
+    end
+
+    respond_to do |format|
+      format.html # Follow regular flow of Rails
+      format.text { render partial: "tasks/content", locals: { tasks: @tasks }, formats: [:html] }
+    end
   end
 
   def show
@@ -23,6 +34,6 @@ class TasksController < ApplicationController
   private
 
   def set_couple
-    @couple = Couple.find(params[:couple_id])
+    @couple = current_user.couple
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,11 +16,11 @@ Rails.application.routes.draw do
       get :score_dashboard
     end
     resources :messages, only: [:index, :create]
-    resources :tasks
     resources :rewards, except: [:destroy]
   end
 
   resources :generic_tasks, except: [:show]
   resources :generic_rewards, only: [:index, :update, :destroy]
+  resources :tasks
 
 end
