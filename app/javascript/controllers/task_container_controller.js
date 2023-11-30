c80c298ab3decae0dae66022e63704cd0aee4aae
Tudal Haccandy <tud.hak_alt@outlook.com>
2023-11-30 11:20:27 +0100

    some minor changes

--- app/javascript/controllers/task_container_controller.js
+++ app/javascript/controllers/task_container_controller.js
@@ -2,26 +2,20 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="task-container"
 export default class extends Controller {
-  static targets = ["statueInput", "assignInput", "form"]
+  static targets = ["statueInput", "assignInput", "form", "content"]
 
   connect() {
-    // console.log(this.element)
-    // console.log(this.statueInputTarget)
-    // console.log(this.assignInputTarget)
-    // console.log(this.formTarget)
   }
 
-  reveal(event) {
-    event.preventDefault()
-    console.log("coucou")
+  reveal() {
     const url = `/tasks?my_params%5Bassigned_to%5D=${this.assignInputTarget.value}&my_params%5Bstatue%5D=${this.statueInputTarget.value}&commit=Save+My+params`
-    console.log(this.formTarget.action)
-    console.log(url)
+    // console.log(this.formTarget.action)
+    // console.log(url)
     fetch(url, {headers: {"Accept": "text/plain"}})
     .then(response => response.text())
     .then((data) => {
-      console.log(data)
-      console.log(this.contentTarget)
+      // console.log(data)
+      // console.log(this.contentTarget)
       this.contentTarget.outerHTML = data
     })
   }
