a522fe33b29935ddb89e2382a34fec7800e98fe2
Tudal Haccandy <tud.hak_alt@outlook.com>
2023-12-04 12:41:33 +0100

    fixes to generic tasks edit

---
+++ app/javascript/controllers/edit_generic_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="edit-generic"
+export default class extends Controller {
+  static targets = ["button", "editForm"]
+
+  connect() {
+  }
+
+  display() {
+    this.editFormTarget.classList.toggle("d-none");
+  }
+}
