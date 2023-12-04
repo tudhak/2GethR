326bcd9dbcec3ad0591b2a4a79c2ce5a348c9c2c
jeremy madaule <jeremy.madaule@gmail.com>
2023-12-04 13:47:24 +0100

    lundi am

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_03_160510) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_04_120203) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -142,6 +142,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_03_160510) do
     t.string "mode"
     t.bigint "couple_id"
     t.string "received_actions"
+    t.string "wishlist"
     t.index ["couple_id"], name: "index_users_on_couple_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
