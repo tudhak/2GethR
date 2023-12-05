1d5d5a77e589484bab5323c9f33aec0dbfc95200
Tudal Haccandy <tud.hak_alt@outlook.com>
2023-12-05 18:05:30 +0100

    calendar back-end completed

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,6 +2,15 @@ class CalendarsController < ApplicationController
   def index
     @tasks = Task.where(user: User.where(couple_id: current_user.couple))
     @rewards = Reward.where(user: User.where(couple_id: current_user.couple))
-    # @couple = current_user.couple
+
+    if params.present?
+      @tasks = Task.where(user: User.where(couple_id: current_user.couple), date: params[:date])
+      @rewards = Reward.where(user: User.where(couple_id: current_user.couple), date: params[:date])
+    end
+
+    respond_to do |format|
+      format.html
+      format.text { render partial: "calendars/events", locals: { tasks: @tasks, rewards: @rewards }, formats: [:html] }
+    end
   end
 end
--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -2,18 +2,36 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="calendar"
 export default class extends Controller {
-static targets = ["targetDate"]
+static targets = ["calendarTitle", "events", "targetDate", "taskDates", "rewardDates"]
 
   connect() {
-  }
+    const taskDates = this.taskDatesTarget.value.slice(0, -1).substr(1).replaceAll('"','').split(', ')
+    const rewardDates = this.rewardDatesTarget.value.slice(0, -1).substr(1).replaceAll('"','').split(', ')
+    for (const day of this.targetDateTargets) {
+      let fullDay = day.innerHTML
+      if (Number(fullDay) >= 1 && Number(fullDay) <= 9) {
+        fullDay = `0${fullDay}`
+      }
+      fullDay = `${this.calendarTitleTarget.dateTime}-${fullDay}`;
+      // console.log(fullDay);
+      if (taskDates.includes(fullDay) || rewardDates.includes(fullDay)) {
+        day.classList.add("selected")
+      }
+      }
+    }
 
   showEvents(event) {
-    console.log(event.currentTarget)
-    // const url = `/calendars?assigned_to%=${this.element.datee}&my_params%5Bstatus%5D=${this.statusInputTarget.value}&commit=Save+My+params`
-    // fetch(url, {headers: {"Accept": "text/plain"}})
-    // .then(response => response.text())
-    // .then((data) => {
-    //   this.contentTarget.outerHTML = data
-    // })
+    let day = event.currentTarget.innerHTML;
+    if (Number(day) >= 1 && Number(day) <= 9) {
+      day = `0${day}`
+    }
+    const date = `${this.calendarTitleTarget.dateTime}-${day}`;
+    // console.log(date)
+    const url = `/calendars?date=${date}`
+    fetch(url, {headers: {"Accept": "text/plain"}})
+    .then(response => response.text())
+    .then((data) => {
+      this.eventsTarget.outerHTML = data
+    })
   }
 }
