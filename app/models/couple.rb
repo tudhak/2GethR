b65d0143d6e40788f313ee91fea2c8ae86c94e40
Tudal Haccandy <tud.hak_alt@outlook.com>
2024-02-16 08:38:08 +0100

    removed unnecessary users-controller

--- app/models/couple.rb
+++ app/models/couple.rb
@@ -3,4 +3,6 @@ class Couple < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :generic_tasks, dependent: :destroy
   has_many :generic_rewards, dependent: :destroy
+
+  generates_token_for :check_couple
 end
