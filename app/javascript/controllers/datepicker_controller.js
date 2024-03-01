f800b17ba55786e84f1f97becad203ed572f5f3e
Tudal Haccandy <tud.hak_alt@outlook.com>
2024-03-01 08:53:10 +0100

    added minDate filtering to datepicker

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,11 +1,12 @@
-import { Controller } from "@hotwired/stimulus"
-import flatpickr from "flatpickr"
+import { Controller } from "@hotwired/stimulus";
+import flatpickr from "flatpickr";
 
 // Connects to data-controller="datepicker"
 export default class extends Controller {
   connect() {
     flatpickr(this.element, {
-      disableMobile: "true"
-    })
+      minDate: "today",
+      disableMobile: "true",
+    });
   }
 }
