6caf4ec79084ac78601a4b09074bf0d03310fa3d
Tudal Haccandy <tud.hak_alt@outlook.com>
2024-04-08 11:21:30 +0200

    added couple token copy to clipboard feature

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -12,45 +12,45 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
-    super do
-    # @user = User.new(user_params)
-    # @user.score = 0
-    # if params[:token].present?
-    #   @couple = Couple.find_by_token_for(:check_couple, params[:token])
-    #   @user.couple = @couple
-    # else
-    #   @couple = Couple.new(couple_params)
-    #   @couple.token = @couple.generate_token_for(:check_couple)
-    #   if @couple.save
-    #     @user.couple = @couple
-    #   else
-    #     flash[:alert] = "Your account could not be created. Please review the form."
-    #     render :new, status: :unprocessable_entity
-    #     return
-    #   end
-    # end
-    # if @user.save
-    #   redirect_to couple_path(@couple)
-    #   flash[:notice] = "Account successfully created!"
-    # else
-    #   render :new, status: :unprocessable_entity
-    #   flash[:alert] = "Your account could not be created. Please review the form."
-    # end
-
-      build_resource(configure_sign_up_params)
-      resource.score = 0
-      resource.save
-      if resource.persisted?
-        # redirect_to couple_path(@couple)
-        set_flash_message! :notice, :signed_up
-        sign_up(resource_name, resource)
-        respond_with resource, location: after_sign_up_path_for(resource)
-        flash[:notice] = "Account successfully created!"
+    # super do
+    @user = User.new(user_params)
+    @user.score = 0
+    if params[:token].present?
+      @couple = Couple.find_by_token_for(:check_couple, params[:token])
+      @user.couple = @couple
+    else
+      @couple = Couple.new(couple_params)
+      @couple.token = @couple.generate_token_for(:check_couple)
+      if @couple.save
+        @user.couple = @couple
       else
-        render :new, status: :unprocessable_entity
         flash[:alert] = "Your account could not be created. Please review the form."
+        render :new, status: :unprocessable_entity
+        return
       end
     end
+    if @user.save
+      redirect_to couple_path(@couple)
+      flash[:notice] = "Account successfully created!"
+    else
+      render :new, status: :unprocessable_entity
+      flash[:alert] = "Your account could not be created. Please review the form."
+    end
+
+      # build_resource(configure_sign_up_params)
+      # resource.score = 0
+      # resource.save
+      # if resource.persisted?
+      #   # redirect_to couple_path(@couple)
+      #   set_flash_message! :notice, :signed_up
+      #   sign_up(resource_name, resource)
+      #   respond_with resource, location: after_sign_up_path_for(resource)
+      #   flash[:notice] = "Account successfully created!"
+      # else
+      #   render :new, status: :unprocessable_entity
+      #   flash[:alert] = "Your account could not be created. Please review the form."
+      # end
+    # end
   end
 
   # GET /resource/edit
@@ -80,9 +80,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password attribute password_confirmation first_name last_name nickname date_of_birth mode photo])
-  end
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password attribute password_confirmation first_name last_name nickname date_of_birth mode photo])
+  # end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
---
+++ app/javascript/controllers/clipboard_controller.js
@@ -0,0 +1,19 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="clipboard"
+export default class extends Controller {
+  static targets = ["token", "button"];
+  static values = {
+    feedbackText: String,
+  };
+
+  // connect() {
+  // }
+
+  copy() {
+    const coupleToken = this.tokenTarget.innerText;
+    navigator.clipboard.writeText(coupleToken);
+    this.buttonTarget.innerText = this.feedbackTextValue;
+    this.buttonTarget.disabled = true;
+  }
+}
