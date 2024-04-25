e8cdc712ac20a732842ff9dc62d3da0fb6133f8d
Tudal Haccandy <tud.hak_alt@outlook.com>
2024-04-25 12:38:08 +0200

    fixed unnecessary password presence validation in User.rb model since it is already managed by Devise and prevented User instances from being updated without password

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :email, :nickname, presence: true, uniqueness: true
-  validates :password, :last_name, :first_name, :date_of_birth, presence: true
+  validates :last_name, :first_name, :date_of_birth, presence: true
 
   # TODO: Remplacer les variables @partner par la méthode user.mate ?
   def mate
