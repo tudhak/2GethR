9f18ad49c6acd60e31dc0cc4319c1de47d2e4c15
Tudal Haccandy <tud.hak_alt@outlook.com>
2024-07-26 16:56:24 +0200

    Added Ruby built-in basic back-end email validation and stronger password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,21 +9,31 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, password_length: 10..128
 
-  validates :email, :nickname, presence: true, uniqueness: true
+  # The built-in email validation regexp below is not bulletproof, but no regexp is
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: "Please enter a valid email" }
+
+  validates :nickname, presence: true
   validates :last_name, :first_name, :date_of_birth, presence: true
+
+  validate :password_complexity
 
   before_create :default_points
 
   private
 
-  # TODO: Remplacer les variables @partner par la méthode user.mate ?
-  def mate
-    couple.users.find { |u| u.id != id }
-  end
-
   def default_points
     self.score = 0
   end
+
+  def password_complexity
+    # Think about using a dedicated third-party solution for password validation
+    # Such as https://github.com/bdmac/strong_password or
+    # devise security extension https://github.com/devise-security/devise-security
+    return if password.blank? || password =~ /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/
+
+    errors.add :password, 'Complexity requirement not met. Please use: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+  end
+
 end
